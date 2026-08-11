f29e97f1a62215cf62b2dd2a24b6d59fe2c007ef
JASKIRAT SINGH KHOKHAR <mr.jaskirat07@gmail.com>
2026-08-11 20:22:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -117,7 +117,7 @@
       els.resultTitle.textContent = "NOT SPAM";
       els.resultFormula.textContent = "S = FALSE";
       els.resultExplain.textContent =
-        "All four spam conditions are FALSE, therefore the email is NOT SPAM";
+        "All four spam conditions are FALSE, therefore the email is NOT SPAM.";
       els.resultReasons.innerHTML = "";
     }
   }
